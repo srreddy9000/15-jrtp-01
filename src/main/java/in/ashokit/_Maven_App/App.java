2e6481f4d79a84392fb2b9db252c4cce3b8e4ede
Srinivas <srreddy9000@gmail.com>
2021-05-24 21:51:40 +0530

    updated

---
+++ src/main/java/in/ashokit/_Maven_App/App.java
@@ -0,0 +1,13 @@
+package in.ashokit._Maven_App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
